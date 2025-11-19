be700fe6a2c6a6191b153d28f9f8dda4e98f7bd5
Flames <vibe-coder@flames.com>
2025-11-19 07:54:36 +0000

    Update project prj_qvx133cu - 2025-11-19T07:54:35.366Z

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Menu, X, DiscordLogoIcon } from 'lucide-react'
+import { Menu, X } from 'lucide-react'
 
 function Navbar() {
   const [open, setOpen] = useState(false)
